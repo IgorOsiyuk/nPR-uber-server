1581f21ea0e79d6a6b41ebe0cf7ca00e86fe0697
IgorOsiyuk <ihar@prezzee.com>
2022-02-06 10:51:15 +0300

    generate verification key

---
+++ src/entities/Verification.ts
@@ -0,0 +1,34 @@
+import { verificationTarget } from "src/types/types";
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, BeforeInsert } from 'typeorm';
+
+@Entity()
+class Verification extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  target: verificationTarget;
+
+  @Column({ type: "text" })
+  payload: string;
+
+  @Column({ type: "text" })
+  key: string;
+
+  @Column({ type: "boolean", default: false })
+  used: string;
+
+  @CreateDateColumn() createAt: string;
+  @UpdateDateColumn() updateAt: string;
+
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === "PHONE") {
+      this.key = Math.floor(Math.random() * 100000).toString();
+
+    } else if (this.target === "EMAIL") {
+      this.key = Math.random().toString(36).slice(2);
+    }
+  }
+}
+
+export default Verification;
